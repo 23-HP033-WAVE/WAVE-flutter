8cde8ed86a5f03f25a7c48cc2b98515438af20b8
daninld <daninld042@gmail.com>
2023-09-10 17:50:08 +0900

    ➕ Feat: signup model 추가

--- lib/screen/signup.dart
+++ lib/screen/signup.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:wave/models/signup_model.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,29 +35,30 @@ class SignUpScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 40),
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: usernameController,
+              decoration: const InputDecoration(
                 labelText: '이름',
               ),
             ),
             const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(
                 labelText: '이메일',
               ),
             ),
             const SizedBox(height: 20),
-            const TextField(
-              decoration: InputDecoration(
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(
                 labelText: '비밀번호',
               ),
               obscureText: true,
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // 회원가입 버튼 클릭 시
-              },
+              onPressed: () => _signUp(context), // _signUp 함수 호출
               style: ElevatedButton.styleFrom(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(25),
@@ -60,5 +73,66 @@ class SignUpScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> _signUp(BuildContext context) async {
+    final username = usernameController.text;
+    final email = emailController.text;
+    final password = passwordController.text;
+
+    // 서버쪽 URL
+    final url = Uri.parse('https://어쩌구 저쩌구/signup');
+
+    final signUpModel =
+        SignUpModel(username: username, email: email, password: password);
+
+    // ScaffoldMessenger를 이용하여 SnackBar를 표시
+    final scaffoldContext = ScaffoldMessenger.of(context);
+    scaffoldContext.showSnackBar(
+      const SnackBar(
+        content: Text('회원가입 중...'),
+      ),
+    );
+
+    final response = await http.post(
+      url,
+      body: jsonEncode(signUpModel.toJson()), // JSON 형태로 전송
+      headers: {'Content-Type': 'application/json'}, // 헤더
+    );
+
+    if (response.statusCode == 200) {
+      // http OK 응답 시
+      final responseData = jsonDecode(response.body);
+      final status = responseData['status'];
+      // 회원가입 성공
+      if (status == 'success') {
+        if (!mounted) return; // 비동기 시 async 뒤에 context에 null값 있을 수 있으므로
+        scaffoldContext.showSnackBar(
+          const SnackBar(
+            content: Text('회원가입 성공!'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+        // 회원가입 성공 후 로그인 화면으로 이동
+        Navigator.pop(context);
+      } else {
+        final message = responseData['message'];
+        if (!mounted) return;
+        scaffoldContext.showSnackBar(
+          SnackBar(
+            content: Text('회원가입 실패: $message'),
+            duration: const Duration(seconds: 2),
+          ),
+        );
+      }
+    } else {
+      if (!mounted) return;
+      scaffoldContext.showSnackBar(
+        SnackBar(
+          content: Text('서버 오류: ${response.statusCode}'),
+          duration: const Duration(seconds: 2),
+        ),
+      );
+    }
   }
 }
