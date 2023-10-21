6d1f72cad4d471c1dd12f7accc358f92a75f90f2
daninld <daninld042@gmail.com>
2023-10-21 22:04:05 +0900

    ☑️ Fix: popupmenu

---
+++ lib/widgets/appbar_popupmenu.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String titleText;
+
+  const MyAppBar({super.key, required this.titleText});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(80.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 1.0,
+      toolbarHeight: 80.0,
+      centerTitle: true,
+      title: Text(
+        titleText,
+        style: const TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          height: 1.0,
+        ),
+      ),
+      leading: IconButton(
+        icon: const Icon(
+          Icons.arrow_back_ios_new,
+          color: Colors.black38,
+        ),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      actions: [
+        PopupMenuButton<int>(
+          itemBuilder: (context) => [
+            const PopupMenuItem(
+              value: 1,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.edit_note,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "수정",
+                    style: TextStyle(color: Colors.black),
+                  )
+                ],
+              ),
+            ),
+            const PopupMenuItem(
+              value: 2,
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.delete_outline,
+                    color: Colors.black,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "삭제",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+          ],
+          color: Colors.grey,
+        ),
+      ],
+    );
+  }
+}
