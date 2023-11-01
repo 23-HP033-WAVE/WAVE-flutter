7cfceaa775144ce2b4797e9147fc15209e674a3e
Nyeong <iwbm312@gmail.com>
2023-11-01 21:12:40 +0900

    ➕ Feat: 백엔드 api 테스트 삭제 후 배포된걸로 변경

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,44 +2,25 @@
 
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:wave/models/temp_webtoon_model.dart';
+import 'package:wave/models/get_reportpost_model.dart';
 import 'package:wave/models/post_model.dart';
 import 'package:wave/models/login_model.dart';
 
 class ApiService {
-  // url
-  static const String baseUrl =
-      "https://webtoon-crawler.nomadcoders.workers.dev"; // fake api - 임시
-  static const String today = "today";
-  // static const String reports = "posts/list"; // 실제 api path
-
-  //  Future<List<WebtoonModel>> ->   Future<List<ReportPostModel>>
-  static Future<List<WebtoonModel>> getReports() async {
-    // 임시
-    List<WebtoonModel> webtoonInstances = [];
-    final url = Uri.parse('$baseUrl/$today');
+  static const String reports = "http://3.39.112.66:5000";
+  static Future<List<ReportPostModel>> getReports() async {
+    List<ReportPostModel> reportInstances = [];
+    final url = Uri.parse(reports);
     final response = await http.get(url);
 
     if (response.statusCode == 200) {
-      final List<dynamic> webtoons = jsonDecode(response.body); // 임시
-
-      // 확인용 코드
-      for (var w in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(w));
+      final List<dynamic> reportposts =
+          jsonDecode(utf8.decode(response.bodyBytes));
+      for (var reportpost in reportposts) {
+        reportInstances.add(ReportPostModel.fromJson(reportpost));
       }
-      return webtoonInstances;
-
-      // REAL -> 조금의 수정 필요. future 등
-      // List<ReportPostModel> reportpostInstances = [];
-      // final url = Uri.parse('$baseUrl/$reports');
-      // final response = await http.get(url);
-      // final List<dynamic> reportposts = jsonDecode(response.body);
-      // for (var reportpost in reportposts) {
-      //   final report = ReportPostModel.fromJson(reportpost);
-      //   reportpostInstances.add(ReportPostModel.fromJson(reportpost));
-      //   print(report.subject); // 제목 출력해보기
-      // }
-      // return reportpostInstances;
+      print(reportInstances.runtimeType);
+      return reportInstances;
     }
     throw Error();
   }
